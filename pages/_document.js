4a630ee4b0c077c8bd4af37f52124adc0e10496c
ANANDKUMAR PATEL <anandpatel95374@gmail.com>
2023-05-19 16:58:40 +0530

    Fixed issues

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,7 +8,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src='https://accounts.google.com/gsi/client'/>
+        <Script src='https://accounts.google.com/gsi/client'/>
       </body>
     </Html>
   )
