7992577d695fb610c8aefdd2a5d077f8b9ad39d8
ANANDKUMAR PATEL <anandpatel95374@gmail.com>
2024-02-10 20:41:40 +0530

    Removed: unused api

--- pages/api/googlesignin.js
+++ pages/api/googlesignin.js
@@ -1,38 +1,43 @@
-import Users from "../../models/Users"
-import connectDb from "../../middleware/mongoose"
+import Users from "../../models/Users";
+import connectDb from "../../middleware/mongoose";
 const CryptoJS = require("crypto-js");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const handler = async (req, res) => {
-    if (req.method == 'POST') {
+  if (req.method == "POST") {
+    let user = await Users.findOne({ email: req.body.email });
 
-        let user = await Users.findOne({ "email": req.body.email })
-
-        if (user) {
-                let token = jwt.sign({ success: true, email: user.email, password: user.password }, process.env.JWT_SECRET, { expiresIn: "1d" });
-                res.status(200).json({ success: true, token, email: user.email })
-            
-        } else {
-
-            let ciphertext = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
-            let createUser = new Users({
-                name: req.body.name,
-                email: req.body.email,
-                password: ciphertext
-            })
-            if (createUser.save()) {
-                let token = jwt.sign({ success: true, email: req.body.email, password: req.body.password }, process.env.JWT_SECRET, { expiresIn: "1d" });
-                res.status(200).json({ success: true, token, email: req.body.email })
-            } else {
-                res.status(400).json({ success: false })
-            }
-        }
-
-
+    if (user) {
+      let token = jwt.sign(
+        { success: true, email: user.email, password: user.password },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.status(200).json({ success: true, token, email: user.email });
+    } else {
+      let ciphertext = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
+      let createUser = new Users({
+        name: req.body.name,
+        email: req.body.email,
+        password: ciphertext,
+      });
+      if (createUser.save()) {
+        let token = jwt.sign(
+          { success: true, email: req.body.email, password: req.body.password },
+          process.env.JWT_SECRET,
+          { expiresIn: "1d" }
+        );
+        res.status(200).json({ success: true, token, email: req.body.email });
+      } else {
+        res.status(400).json({ success: false });
+      }
     }
-    else {
-        res.status(400).json({ error: "This method is not allowed" })
-    }
-}
+  } else {
+    res.status(400).json({ error: "This method is not allowed" });
+  }
+};
 
-export default connectDb(handler)
+export default connectDb(handler);
--- pages/api/sendresetlink.js
+++ pages/api/sendresetlink.js
@@ -1,38 +1,40 @@
-import Users from "../../models/Users"
-import Token from "../../models/Token"
-import connectDb from "../../middleware/mongoose"
-const jwt = require('jsonwebtoken');
+import Users from "../../models/Users";
+import Token from "../../models/Token";
+import connectDb from "../../middleware/mongoose";
+const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
 const handler = async (req, res) => {
-    if (req.method == 'POST') {
-        try {
-            let user = await Users.findOne({ email: req.body.email })
+  if (req.method == "POST") {
+    try {
+      let user = await Users.findOne({ email: req.body.email });
 
-            if (user) {
-                let token = await Token.findOne({ email: req.body.email });
-                if (!token) {
-                    token = await new Token({
-                        email: req.body.email,
-                        token: jwt.sign({ email: req.body.email}, process.env.JWT_SECRET, { expiresIn: "1d" })
-                    }).save();
-                }
-                const url = `${process.env.NEXT_PUBLIC_HOST}/resetpassword?resettoken=${token.token}`
-                let transporter = nodemailer.createTransport({
-                    service: "​gmail",
-                    port: 587,
-                    auth: {
-                        user: process.env.EMAIL,
-                        pass: process.env.EMAIL_PASS
-                    }
-                });
+      if (user) {
+        let token = await Token.findOne({ email: req.body.email });
+        if (!token) {
+          token = await new Token({
+            email: req.body.email,
+            token: jwt.sign({ email: req.body.email }, process.env.JWT_SECRET, {
+              expiresIn: "1d",
+            }),
+          }).save();
+        }
+        const url = `${process.env.NEXT_PUBLIC_HOST}/resetpassword?resettoken=${token.token}`;
+        let transporter = nodemailer.createTransport({
+          service: "​gmail",
+          port: 587,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS,
+          },
+        });
 
-                //​Create the message object​
-                let message = {
-                    from: process.env.EMAIL,
-                    to: user.email,
-                    subject: "Onstore reset password request",
-                    html: `
+        //​Create the message object​
+        let message = {
+          from: process.env.EMAIL,
+          to: user.email,
+          subject: "Onstore reset password request",
+          html: `
                     <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
                     style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
                     <tr>
@@ -44,8 +46,8 @@ const handler = async (req, res) => {
                                 </tr>
                                 <tr>
                                     <td style="text-align:center;">
-                                      <a href="https://onstore.vercel.app" title="logo" target="_blank">
-                                        <img width="60" src="https://firebasestorage.googleapis.com/v0/b/fir-243cd.appspot.com/o/App%2Flogo-6.png?alt=media&token=16b005d6-4496-4187-8733-984b49e45fe3" title="logo" alt="logo" style="width: 215px; height:100px;">
+                                      <a href="https://onstore.vercel.app" title="onstore-logo" target="_blank">
+                                        <img width="60" src="https://res.cloudinary.com/dgagdyxyy/image/upload/f_auto,q_auto/gpod1wvczslirfu2fa9w" title="onstore-logo" alt="onstore-logo" style="width: 215px; height:100px;">
                                       </a>
                                     </td>
                                 </tr>
@@ -99,30 +101,46 @@ const handler = async (req, res) => {
                             </table>
                         </td>
                     </tr>
-                </table>`
-                    //​ html: "<p>HTML version of the message</p>"​
-                };
+                </table>`,
+          //​ html: "<p>HTML version of the message</p>"​
+        };
 
-                //​Send mail​
-                await transporter.sendMail(message, (err) => {
-                    if (err) {
-                        success = false
-                        res.status(400).json({ success: false, message: "There is Some Error occured to sent a email" })
-                    }
-                    else {
-                        res.status(200).json({ success: true, message: "Reset link successfully sent to your email" })
-                        return
-                    }
-                })
-            } else {
-                res.status(400).json({ success: false, message: "User with given email does not exist" })
-            }
-        } catch (error) {
-            res.status(400).json({ success: false, message: "Some error occured, Try again!" })
-        }
-    } else {
-        res.status(400).json({ error: "This method is not allowed" })
+        //​Send mail​
+        await transporter.sendMail(message, (err) => {
+          if (err) {
+            success = false;
+            res
+              .status(400)
+              .json({
+                success: false,
+                message: "There is Some Error occured to sent a email",
+              });
+          } else {
+            res
+              .status(200)
+              .json({
+                success: true,
+                message: "Reset link successfully sent to your email",
+              });
+            return;
+          }
+        });
+      } else {
+        res
+          .status(400)
+          .json({
+            success: false,
+            message: "User with given email does not exist",
+          });
+      }
+    } catch (error) {
+      res
+        .status(400)
+        .json({ success: false, message: "Some error occured, Try again!" });
     }
-}
+  } else {
+    res.status(400).json({ error: "This method is not allowed" });
+  }
+};
 
-export default connectDb(handler)
+export default connectDb(handler);
